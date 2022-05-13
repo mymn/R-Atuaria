340beb53c9caa072939bb37bf56f422f4bdc7753
mymn <105239736+mymn@users.noreply.github.com>
2022-05-13 15:59:14 -0300

    Update GGD at49.R

--- GGD at49.R
+++ GGD at49.R
@@ -9,14 +9,13 @@ AT49 <- read_excel("D:/BrowserDownloads/P1 - at49.xlsx")
 #Alterar o caminho para o local da planilha
 #AT49M = x,4
 #AT49F = x 8
-
+AT49<-as.data.frame(AT49)
 
 param_GGD=function(){
-  param=c()
-  param[1]=runif(n=simnum,min=0.0003,max=0.0004)
-  param[2]=runif(n=simnum,min=1.2,max=2.0)
-  param[3]=runif(n=simnum,min=0.06,max=0.07)
-  return(param)
+  as.vector(param)
+  param[1]<-runif(n=simnum,min=0.0003,max=0.0004)
+  param[2]<-runif(n=simnum,min=1.2,max=2.0)
+  param[3]<-runif(n=simnum,min=0.06,max=0.07)
 }
 #funcao para criar os parametros da GGD
 #onde param[1]=theta, param[2]=lambda e param[3]=c
@@ -24,42 +23,61 @@ param_GGD=function(){
 simnum=1000
 #numero de simulacoes 
 
-
-
 survestim=function(omega,param_GGD,simnum){
-  Estim=c()
+  as.vector(Estim)
   param_GGD()
-  for(j in j:omega){
-    Estim[j]=Estim[j]+(1-(1-exp((-param[2]/param[3])*(exp(param[3]*(j-1)-1)))^param[1]))             
+  for(i in 1:simnum){
+    for(j in 1:omega){
+      Estim[j]<-Estim[j]+as.numeric((1-(1-exp((-param[2]/param[3])*(exp(param[3]*(j-1)-1)))^param[1])));Estim         
+    }
   }
   for(i in 1:omega){
-    Estim[i]=Estim[i]/simnum
+    Estim[i]<-Estim[i]/simnum;Estim
   }
-return(Estim)
 }
 #funcao para calculo da curva estimada de sobrevivencia por monte carlo
 
 beneficio=function(AT49,omega,survestim,param_GGD){
   bene = 0
   for(j in 1:110){
-    i = AT49[j,13]
-    while(i < omega){
-      bene=bene+(Estim[i]*AT49[j,14]*AT49[i,15])
-      i =i+1
+    idade = AT49[j,13]
+    while(idade < omega){
+      bene=bene+(Estim[i]*AT49[j,14]*AT49[i,15]);bene
+      idade =idade+1
     }
   }
-  return(bene)
 }
 #calculo da reserva da carteira BD em 1 iteracao
 
 repeticoes=50
-somareserva=function(beneficio,survestim){
+somareserva=function(beneficio,survestim,repeticoes){
   reserva=c()
   for(i in 1:repeticoes){
-    survestim()
-    reserva=c(reserva,beneficio)
+    survestim(omega,param_GGD,simnum)
+    reserva=c(reserva,bene[i])
+    print(reserva[i])
   }
-  return(reserva)
 }
-ggplot(reserva)+scale_y_continuous(labels=percent)+labs(x='Reserva',y='Porcentagem')
 
+hist(reserva)
+ggplot(somareserva)+scale_y_continuous(labels=percent)+labs(x='Reserva',y='Porcentagem')
+
+#9 e 10 para M, 11 e 12 para F (Dx e Nx)
+#13 para idade 14 beneficio
+
+somaM=0 #AT49M
+somaF=0 #AT49F
+reservatabM=function(omega,AT49){
+  for(j in 1:omega){
+    idade=AT49[j,13]
+    somaM=somaM+AT49[idade,9]/AT49[idade,10]*AT49[idade,14]
+  }
+  return(somaM)
+}
+reservatabF=function(omega,AT49){
+  for(j in 1:omega){
+    idade=AT49[j,13]
+    somaF=somaF+AT49[idade,11]/AT49[idade,12]*AT49[idade,14]
+  }
+  return(somaF)
+}
