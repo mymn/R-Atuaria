f2e41b8342525f772e0fae29a22812a6223a80c2
mymn <105239736+mymn@users.noreply.github.com>
2022-05-31 16:35:26 -0300

    Update Vida2trabalho.R

--- Vida2trabalho.R
+++ Vida2trabalho.R
@@ -20,5 +20,32 @@ IdadeDependente=readline(prompt = "Idade do dependente: ")
 SexoTitular=readline(prompt = "Sexo do titular(M ou F): ")
 SexoConjuge=readline(prompt = "Sexo do conjuge(M ou F): ")
 SexoDependente=readline(prompt = "Sexo do depentente(M ou F): ")
-TaxaJuros=readline(prompt = "Taxa de juros(decimal): ")
+jaxajuros=readline(prompt = "Taxa de juros(decimal): ")
 Tabua=readline(prompt = "Tabua(1=AT2000,2=B...,3=...): ")
+nvidas=readline(prompt = "Numero de vidas(2 ou 3): ")
+
+agemax=tail(tabua[,1],n=1) #ultima idade da tabua utilizada
+#inicializacao dos vetores de comutacao
+idade=c(1:agemax)
+l=vector(length=agemax)
+d=vector(length=agemax)
+vx=vector(length=agemax)
+D=vector(length=agemax)
+N=vector(length=agemax)
+C=vector(length=agemax)
+M=vector(length=agemax)
+p=vector(length=agemax)
+q=vector(length=agemax)
+
+
+if(nvidas==2){
+  #criando dataframes
+  duasvidasX=data.frame(idade,q,l,d,vx,D,N,C,M)
+  duasvidasY=data.frame(idade,q,l,d,vx,D,N,C,M)
+  duasvidasXY=data.frame(idade,p,l,d,vx,D,N,C,M)
+  #renomear colunas
+  colnames(duasvidasX)=c("Idade","qx","lx","dx","v^x","Dx","Nx","Cx","Mx")
+  colnames(duasvidasY)=c("Idade","qy","ly","dy","v^y","Dy","Ny","Cy","My")
+  colnames(duasvidasXY)=c("Idade","pxy","lxy","dxy","v^t","Dxy","Nxy","Cxy","Mxy")
+  agemax=tail(duasvidasX[,1],n=1)
+}
