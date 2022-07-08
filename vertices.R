8e417bf2e9b777481a262ead83a52e100a80ec0f
mymn <105239736+mymn@users.noreply.github.com>
2022-07-08 20:38:24 -0300

    Update vertices.R

--- vertices.R
+++ vertices.R
@@ -1,4 +1,5 @@
 #Ler todas funcoes, alterar o local da entrada dos dados e rodar a funcao main
+#inserir vertices
 
 library(Rfast)
 library(readxl)
@@ -6,8 +7,9 @@ library(readxl)
 #importar planilha com os fluxos e os vertices
 importardados=function(){
   #alterar local da planilha com as entradas
-  planilha<-read_excel("local da planilha")
-  nfluxos=length(planilha[datacompra])
+  planilha<-read_excel("C:/Users/aluno1/Downloads/CarteiraParaSimularemP2.xlsx")
+  planilha=planilha[-c(1),]
+  nfluxos=nrow(planilha[,1])
   nvertices=length(planilha[data_vertice])
   vertices[,1]=planilha[,data_vertice]
   vertices[,2]=0
@@ -29,15 +31,16 @@ Tojulian=function(calendr){ #transformar data em data juliana
 #vetor teste para fluxo                                                  #
 #fluxo[,1]=c(45500,45700,46000,46100,45550)                              #
 #fluxo[,2]=c(1,2,3,4,5)                                                  #
-#preencher os vertices para teste                                        #
+#preencher os vertices para teste      
+#nvertices=5
 #vertices[,1]=c(44710,45075,45440,45805,46170) #inserir as datas         #
 #vertices[,2]=0                                                          #
 ##########################################################################
 
 #distribuicao nos vertices
 distribuicao=function(nfluxos,nvertices,vertices,fluxo){
-#vetor auxiliar para armazenar as "distancias" entre as datas de 
-# #vencimento dos fluxos e dos vertices  
+  #vetor auxiliar para armazenar as "distancias" entre as datas de 
+  # #vencimento dos fluxos e dos vertices  
   aux=vector(length=nvertices)
   for(j in 1:nfluxos){
     aux=numeric(nvertices)
@@ -47,13 +50,13 @@ distribuicao=function(nfluxos,nvertices,vertices,fluxo){
       #indices dos 2 menores valores no vetor auxiliar(a,b)
       b=nth(as.numeric(aux), 2, descending = F,na.rm=T,index.return=T)
       a=nth(as.numeric(aux), 1, descending = F,na.rm=T,index.return=T)}
-      #distribuicao proporcional nos vertices
-      vertices[b,2]=vertices[b,2]+((abs(fluxo[j,1]-vertices[a,1]))/abs((vertices[a,1]-vertices[b,1])))*fluxo[j,2]
-      vertices[a,2]=vertices[a,2]+((abs(fluxo[j,1]-vertices[b,1]))/abs((vertices[a,1]-vertices[b,1])))*fluxo[j,2]
+    #distribuicao proporcional nos vertices
+    vertices[b,2]=vertices[b,2]+((abs(fluxo[j,1]-vertices[a,1]))/abs((vertices[a,1]-vertices[b,1])))*fluxo[j,2]
+    vertices[a,2]=vertices[a,2]+((abs(fluxo[j,1]-vertices[b,1]))/abs((vertices[a,1]-vertices[b,1])))*fluxo[j,2]
   }
 }
 
-#criação da matriz carteira
+#criaÃ§Ã£o da matriz carteira
 criarestruturas=function(){
   #criacao do vetor de fluxos
   fluxo=matrix(nrow=nfluxos,ncol=2)#[tempo,valor]tempo em data juliana
@@ -62,7 +65,7 @@ criarestruturas=function(){
   vertices=matrix(nrow=nvertices,ncol=2) #[tempo,valor]
   colnames(vertices) <- c("Data juliana", "Valor")
   #criacao da matriz para o calculo da carteira
-  carteira=matrix(ncol=3,nrows=nfluxos)
+  carteira=matrix(nrow=nfluxos,ncol=3,)
   colnames(carteira)=c("Dias Uteis compra ao venc","VF no venc","Dias Uteis hoje ao venc")
 }
 
@@ -76,11 +79,11 @@ calculocarteira=function(curva){
   hoje=Tojulian(Sys.Date())
   curva()
   for(i in 1:nfluxos){
-      carteira[1,i]=Tojulian(planilha[datavencimento])-Tojulian(planilha[datacompra])*252/365
-      carteira[3,i]=Tojulian(planilha[datavencimento])-Tojulian(hoje)*252/365
-      carteira[2,i]=planilha[quantidade]*1000
-      fluxo[i,1]=planilha[Tojulian(datavencimento)]
-      fluxo[i,2]=carteira[2,i]*(1/(1+rtanual[i])^carteira[3,i])
+    carteira[1,i]=Tojulian(planilha[,5])-Tojulian(planilha[,3])*252/365
+    carteira[3,i]=Tojulian(planilha[,5])-Tojulian(hoje)*252/365
+    carteira[2,i]=planilha[,4]*1000
+    fluxo[i,1]=Tojulian(planilha[,5])
+    fluxo[i,2]=carteira[2,i]*(1/(1+rtanual[i])^carteira[3,i])
   }
 }
 
@@ -119,9 +122,9 @@ main=function(){
   for(i in 1:nsim){
     calculocarteira(curva)
     distribuicao(nfluxos,nvertices,vertices,fluxo)
-    resultado[,i]=vertices
+    resultado[,i]=vertices[]
   }
-  plot(resultado)
+  hist(resultado)
 }
 
 main()
